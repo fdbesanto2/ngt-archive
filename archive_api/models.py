aa3bb64999b02ec31e0b1fdd251aac9a724dd936
Val Hendrix <vchendrix@lbl.gov>
2016-10-26 18:42:55 -0700

    Allow Multiple contacts/PIs per site
    Resolves #11
    
    + This will require the database to be dumped, dropped, recreated  and loaded with the massaged data.
    + The dumped JSON data will need to be edited to have a list of contacts and pis per site.

--- archive_api/models.py
+++ archive_api/models.py
@@ -63,8 +63,8 @@ class Site(models.Model):
     location_bounding_box_lr_latitude = models.FloatField(blank=True, null=True)
     location_bounding_box_lr_longitude = models.FloatField(blank=True, null=True)
     site_urls = models.TextField(blank=True, null=True)
-    contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING, blank=True, null=True)
-    pi = models.ForeignKey(Contact, on_delete=models.DO_NOTHING, related_name='+', blank=True, null=True)
+    contact = models.ManyToManyField(Contact)
+    pi = models.ManyToManyField(Contact, related_name='+')
     submission = models.ForeignKey(Contact, on_delete=models.DO_NOTHING, related_name='+')
     submission_date = models.DateField(blank=True, null=True)
 
