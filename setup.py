9c443267c2808ad320ce19fb1b88173ce200915d
Val Hendrix <vchendrix@lbl.gov>
2016-12-15 12:22:56 -0800

    Version tagging functionaltiy
    Resolves #54

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
 import sys
+import subprocess
+import os
+
+# Update version from latest git tags.
+# Create a version file in the root directory
+version_py = os.path.join(os.path.dirname(__file__), 'ngt_archive/version.py')
+try:
+    git_describe = subprocess.check_output(["git", "describe", "--tags"]).rstrip().decode('utf-8')
+    version_msg = "# Managed by setup.py via git tags.  **** DO NOT EDIT ****"
+    with open(version_py, 'w') as f:
+        f.write(version_msg + os.linesep + "__version__='" + git_describe.split("-")[0] + "'")
+        f.write(os.linesep + "__release__='" + git_describe + "'" + os.linesep)
+
+except Exception as e:
+    # If there is an exception, this means that git is not available
+    # We will used the existing version.py file
+    pass
+
+with open(version_py) as f:
+    code = compile(f.read(), version_py, 'exec')
+    exec(code)
+
+
 
 from setuptools import setup
 if sys.version_info < (2,6):
@@ -9,7 +32,7 @@ elif sys.version_info < (2,7):
     sys.exit('Sorry, Python < 2.7 is not supported')
 
 setup(name='ngt_archive',
-      version='1.0',
+      version=__release__,
       description='NGEE Tropics Archive Service',
       author='Val Hendrix',
       author_email='vchendrix@lbl.gov',
