22ecf9504e49bf1b8e96e9eddf5afe97761fefbf
Val Hendrix <vchendrix@lbl.gov>
2017-06-16 15:08:22 -0700

    Removed hard coded googlemaps api key
    Closes #307
    
    The following changes will make it possible to generate a new
    Google maps key using an NGEE Tropics google account.
    
    + Created a Django View class for the index page (IndexView)
    + Wrapped a login_required decorator around the IndexView
    + Added config settings.GOOGLE_MAPS_KEY
    + Updated LOGIN_URL in settings to /api/api-auth/login/

--- ui/views.py
+++ ui/views.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'ui/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['google_maps_key'] = settings.GOOGLE_MAPS_KEY
+
+        return context
+
